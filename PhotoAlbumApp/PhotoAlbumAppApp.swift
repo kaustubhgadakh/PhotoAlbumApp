1aae2f7547fe945d750a6bb1c263b7b01764e5ec
Kaustubh Gadakh <kaustubhgadakh2001@gmail.com>
2024-08-10 18:03:11 +0530

    Initial Commit

---
+++ PhotoAlbumApp/PhotoAlbumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumAppApp.swift
+//  PhotoAlbumApp
+//
+//  Created by Kaustubh Kishor Gadakh on 10/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAlbumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoAlbumApp/ViewModels/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  PhotoAlbumApp
+//
+//  Created by Kaustubh Kishor Gadakh on 10/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
